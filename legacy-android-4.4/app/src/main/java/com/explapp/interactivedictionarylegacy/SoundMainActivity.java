44cf4af473f6aa63b32761d7269ec96956ad2632
aljwaal1 <yaya15112016@gmail.com>
2026-07-13 22:35:38 +0300

    fix(android-4.4): match public dictionary lifecycle

--- legacy-android-4.4/app/src/main/java/com/explapp/interactivedictionarylegacy/SoundMainActivity.java
+++ legacy-android-4.4/app/src/main/java/com/explapp/interactivedictionarylegacy/SoundMainActivity.java
@@ -15,7 +15,7 @@ public class SoundMainActivity extends MainActivity {
     private float downY;
     private long lastSoundAt;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         tones = new ToneGenerator(AudioManager.STREAM_MUSIC, 47);
         super.onCreate(savedInstanceState);
     }
